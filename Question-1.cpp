c46d757b72c2cc61e1bda2ebd18a9d27c5e92277
Dwelov <sirajmeher984@gmail.com>
2025-12-07 15:38:50 +0500

    initial commit

---
+++ Question-1.cpp
@@ -0,0 +1,34 @@
+/*Write a program to calculate monthly water bill: up to 25 units cost 30 per unit, above 25 cost 45 per unit for the extra.
+ Read N days, read usage per day, sum and compute final bill. Use variables, IO, loops, if/else. Push code to GitHub and answer with URL.*/
+ 
+#include <iostream>
+#include <string>
+using namespace std;
+ int main(){
+	int num,usagePerDay,sumOfUnits = 0,finalBill=1; //this is for the number of the days
+	//reading the number of the days
+	cout<<"Enter the number of the days: ";
+	cin>>num;
+	
+	//read usage per day
+	for(int i=1;i<=num;i++){
+		cout<<"Day "<<i<<": ";
+		cin>>usagePerDay;
+		sumOfUnits += usagePerDay;
+	}
+	 
+	 //Calculating the final bill
+	 if(sumOfUnits <= 25){
+	 	finalBill = sumOfUnits * 30;
+	 }else{
+	 	//condition-2: Charge the 45 extra for every extra unit than 25
+	 	finalBill = (25 * 30) + ((sumOfUnits-25) * 45);
+	 }
+	  
+	  cout<<endl<<string(40,'-')<<endl;
+	 //Dispalying the result
+	 cout<<"Total Units Consumed: "<<sumOfUnits<<endl
+	 <<"Final Bill is : "<< finalBill<<endl;
+	 return 0;
+	
+}
